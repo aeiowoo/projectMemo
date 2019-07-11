6f678ad1a204c994e233c9d1730f5ba7f5af075d
aeiowoo <aeiowoo@gmail.com>
2019-07-11 20:43:39 +0900

    Initial Commit

---
+++ projectMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectMemo
+//
+//  Created by Jeon Myeong Woo on 2019. 7. 11..
+//  Copyright © 2019년 baekbul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
